20a01a989afc4912c2b73f351f5eec793a376365
rajan pandya <rajankpandya@gmail.com>
2020-04-14 13:43:04 -0400

    basic actions done

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,5 +1,17 @@
-import React from "react";
+import { getInitialData } from '../utils/api';
+import { receiveUsers } from '../actions/users'
+import { receiveQuestions } from '../actions/questions'
+import { setAuthedUser } from '../actions/authedUser'
 
-export default function (){
-    return <p>this is shared action for now</p>
+//TODO: need to fix this hard coded authed_id value to user chosen option
+const AUTHED_ID = 'sarahedo';
+
+export function handleInitialData(){
+    return((dispatch) => {
+        return getInitialData().then(({ users, questions}) => {
+            dispatch(receiveUsers(users));
+            dispatch(receiveQuestions(questions));
+            dispatch(setAuthedUser(AUTHED_ID));
+        })
+    })
 }
