d793a0f61cc866774f3ed99838da36d9d48168b3
rajan pandya <rajankpandya@gmail.com>
2020-04-17 21:59:21 -0400

    vote or view poll

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -12,19 +12,21 @@ class QuestionCard extends React.Component{
                 <Card.Content>
                     <Card.Header>Would you rather? </Card.Header>
                     <Card.Meta> By {theQuestion.author}</Card.Meta>
-                    {/* <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                    </Card.Description> */}
+                    <Card.Description>
+                        {theQuestion.optionOne.text} <br /> or <br /> {theQuestion.optionTwo.text}
+                </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
-                    <div className='ui two buttons'>
-                        <Button basic color='green'>
-                            {theQuestion.optionOne.text}
-                        </Button>
-                        <Button basic color='blue'>
-                            {theQuestion.optionTwo.text}
-                        </Button>
-                    </div>
+                    { answered 
+                    ? (<div className='ui single'>
+                        <Button basic color='blue'>View the Poll</Button>
+                       </div>
+                    )
+                    : (<div className='ui single'>
+                            <Button basic color='green'>Vote</Button>
+                        </div>
+                    )
+                }
                 </Card.Content>
             </Card>
             // <div > {theKeys.filter(k => ['author', 'id'].includes(k)).map(k => <div> <p>{k} : {theQuestion[k]} </p>  </div>)} </div>
